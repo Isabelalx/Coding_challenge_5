556a04573ab1e44475d4054ced5c07d1ec5ec70a
Isabelalx <isabelalopesxavier@usf.edu>
2025-02-09 20:54:52 -0600

    Task 1 - Customer Profile.

---
+++ cc_5.js
@@ -0,0 +1,7 @@
+// Task 1
+
+// Declare an object customer with the properties 
+const customer = { name: "John Doe", age: 35, email: "john.doe@email.com" };
+console.log(`Customer Name: ${customer.name}`);
+console.log(`Customer Age: ${customer.age}`);
+console.log(`Customer Email: ${customer.email}`);
